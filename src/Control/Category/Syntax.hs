a5ba54eeacb33bf055f09aa85280064dd0780dc8
Samuel Gélineau <gelisam+github@gmail.com>
2014-08-23 12:19:42 -0400

    spill where block to top-level, only expose API

--- src/Control/Category/Syntax.hs
+++ src/Control/Category/Syntax.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE TemplateHaskell #-}
-module Control.Category.Syntax where
+module Control.Category.Syntax (getInput, returnC, syntax) where
 
 import Prelude hiding (id, (.))
 
@@ -30,43 +30,43 @@ returnCName = 'returnC
 
 
 syntax :: Q Exp -> Q Exp
-syntax = fmap go
+syntax = fmap categorySyntax
+
+categorySyntax :: Exp -> Exp
+categorySyntax = continue . begin
+
+-- >>> begin [|do x <- getInput
+--                cmds
+--            |]
+-- (x, cmds)
+begin :: Exp -> (Pat, [Stmt])
+begin (DoE (BindS x (VarE getInput):cmds))
+  | getInput == getInputName
+  = (x, cmds)
+begin _ = error "expected $(syntax [|do x <- getInput; ...|])"
+
+-- >>> continue (x, [|y <- foo x; cmds|]
+-- (foo >>> continue (y, cmds))
+continue :: (Pat, [Stmt]) -> Exp
+continue (env, [cmd]) = end (env, cmd)
+continue (env, (cmd:cmds)) = InfixE (Just morph)
+                                    (VarE thenName)
+                                    (Just (continue (env', cmds)))
   where
-    go :: Exp -> Exp
-    go = continue . begin
-    
-    -- >>> begin [|do x <- getInput
-    --                cmds
-    --            |]
-    -- (x, cmds)
-    begin :: Exp -> (Pat, [Stmt])
-    begin (DoE (BindS x (VarE getInput):cmds))
-      | getInput == getInputName
-      = (x, cmds)
-    begin _ = error "expected $(syntax [|do x <- getInput; ...|])"
-    
-    -- >>> continue (x, [|y <- foo x; cmds|]
-    -- (foo >>> continue (y, cmds))
-    continue :: (Pat, [Stmt]) -> Exp
-    continue (env, [cmd]) = end (env, cmd)
-    continue (env, (cmd:cmds)) = InfixE (Just morph)
-                                        (VarE thenName)
-                                        (Just (continue (env', cmds)))
-      where
-        (env', morph) = makeMorphism (env, cmd)
-    
-    -- >>> makeMorphism (x, [|y <- foo x|])
-    -- (y, foo)
-    makeMorphism :: (Pat, Stmt) -> (Pat, Exp)
-    makeMorphism (VarP x, BindS y (AppE morph (VarE x')))
-      | x == x'
-      = (y, morph)
-    makeMorphism _ = error "expected $(syntax [|do ...; x <- foo y; ...|])"
-    
-    -- >>> end (x, [|returnC x|])
-    -- id
-    end :: (Pat, Stmt) -> Exp
-    end (VarP x, NoBindS (AppE morph (VarE x')))
-      | x == x'
-      = morph
-    end _ = error "expected $(syntax [|do ...; returnC x|])"
+    (env', morph) = makeMorphism (env, cmd)
+
+-- >>> makeMorphism (x, [|y <- foo x|])
+-- (y, foo)
+makeMorphism :: (Pat, Stmt) -> (Pat, Exp)
+makeMorphism (VarP x, BindS y (AppE morph (VarE x')))
+  | x == x'
+  = (y, morph)
+makeMorphism _ = error "expected $(syntax [|do ...; x <- foo y; ...|])"
+
+-- >>> end (x, [|returnC x|])
+-- id
+end :: (Pat, Stmt) -> Exp
+end (VarP x, NoBindS (AppE morph (VarE x')))
+  | x == x'
+  = morph
+end _ = error "expected $(syntax [|do ...; returnC x|])"
