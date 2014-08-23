2c37f84cc17882b69c0cf3202867a6da1ca70cfc
Samuel Gélineau <gelisam+github@gmail.com>
2014-08-23 18:47:04 -0400

    scope bounds

--- src/Control/Category/Syntax.hs
+++ src/Control/Category/Syntax.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RecordWildCards, TemplateHaskell #-}
 module Control.Category.Syntax (getInput, returnC, syntax) where
 
 import Prelude hiding (id, (.))
 
 import Control.Category
+import Data.List
 import Language.Haskell.TH
 import Text.Printf
 
@@ -112,6 +113,39 @@ mkPipeline e = Pipeline (mkVarsP x) (map mkStep stmts) lastX lastCmd
         NoBindS e' -> splitStep e'
         _          -> error msg
     msg = printf "expected (returnC ...), got:\n%s" (pprint lastCmd)
+
+
+type Names = [Name]
+type AllVars = (Vars, [Name])
+
+listVars :: Vars -> Names
+listVars (Var x) = [x]
+listVars (Pair x y) = listVars x ++ listVars y
+
+-- Make preconditions list all the variables available at this point,
+-- and postconditions list all the variables needed afterwards.
+allVars :: Pipeline Vars -> Pipeline AllVars
+allVars (Pipeline {..}) = Pipeline (initialCond, noVars)
+                                   intermediateSteps'
+                                   (finalCond, allVars)
+                                   finalCommand
+  where
+    noVars = []
+    (intermediateSteps', allVars) = go noVars intermediateSteps
+    
+    go :: Names -> [Step Vars] -> ([Step AllVars], Names)
+    go prevScope [] = ([], prevScope)
+    go prevScope (step:steps) = (step':steps', newUsedVars)
+      where
+        (steps', futureUsedVars) = go newScope steps
+        
+        Step varsUsedByCmd cmd varsProducedByCmd = step
+        step' = Step (varsUsedByCmd, prevScope)
+                     cmd
+                     (varsProducedByCmd, futureUsedVars)
+        
+        newScope = prevScope ++ listVars varsProducedByCmd
+        newUsedVars = futureUsedVars `union` listVars varsUsedByCmd
 
 
 syntax :: Q Exp -> Q Exp
