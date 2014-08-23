43790107f322c1d061bc3d0d4a7fc61fe1e46e2a
Samuel Gélineau <gelisam+github@gmail.com>
2014-08-23 15:35:31 -0400

    more precise types

--- src/Control/Category/Syntax.hs
+++ src/Control/Category/Syntax.hs
@@ -38,6 +38,82 @@ returnCName :: Name
 returnCName = 'returnC
 
 
+-- A simpler representation for nested tuples of variables.
+data Vars = Var Name | Pair Vars Vars
+
+-- A black box Exp, provided by the user.
+type Cmd = Exp
+
+-- A simpler representation for [|x' <- cmd x|]
+data Step = Step
+  { args    :: Vars -- (x,y,...)
+  , command :: Cmd  -- cmd :: k (a,b,...) (a',b',...)
+  , results :: Vars -- (x',b',...)
+  }
+
+-- A simpler representation for [|do x <- getInput
+--                                   y <- cmd x
+--                                   z <- cmd' y
+--                                   returnC z
+--                               |]
+data Pipeline = Pipeline
+  { firstArgs         :: Vars    -- do (x,y,...) <- getInput
+  , intermediateSteps :: [Step]  --    y <- cmd x
+                                 --    z <- cmd' y
+  , lastArgs          :: Vars    --            z
+  , lastCommand       :: Cmd     --    returnC
+  }
+
+
+mkVarsE :: Exp -> Vars
+mkVarsE (VarE x) = Var x
+mkVarsE (TupE [x,y]) = Pair (mkVarsE x) (mkVarsE y)
+mkVarsE x = error msg
+  where
+    msg = printf "expected a var or a tuple of vars, got %s" (pprint x)
+
+mkVarsP :: Pat -> Vars
+mkVarsP (VarP x) = Var x
+mkVarsP (TupP [x,y]) = Pair (mkVarsP x) (mkVarsP y)
+mkVarsP x = error msg
+  where
+    msg = printf "expected a var or a tuple of vars, got %s" (pprint x)
+
+
+splitStep :: Exp -> (Vars, Cmd)
+splitStep (AppE e x) = (mkVarsE x, e)
+splitStep (InfixE (Just e) (VarE dollar) (Just x))
+  | dollar == dollarName
+  = (mkVarsE x, e)
+splitStep e = error msg
+  where
+    msg = printf "expected (cmd (x,y,...)), got:\n%s" (pprint e)
+
+mkStep :: Stmt -> Step
+mkStep (BindS x' e) = uncurry Step (splitStep e) (mkVarsP x')
+mkStep s = error msg
+  where
+    msg = printf "expected (x <- ...), got:\n%s" (pprint s)
+
+
+splitPipeline :: Exp -> (Pat, [Stmt], Stmt)
+splitPipeline (DoE (BindS x (VarE getInput):stmts))
+  | getInput == getInputName
+  = (x, init stmts, last stmts)
+splitPipeline e = error msg
+  where
+    msg = printf "expected (do x <- getInput; ...), got:\n%s" (pprint e)
+
+mkPipeline :: Exp -> Pipeline
+mkPipeline e = Pipeline (mkVarsP x) (map mkStep stmts) lastX lastCmd
+  where
+    (x, stmts, lastStmt) = splitPipeline e
+    (lastX, lastCmd) = case lastStmt of
+        NoBindS e' -> splitStep e'
+        _          -> error msg
+    msg = printf "expected (returnC ...), got:\n%s" (pprint lastCmd)
+
+
 syntax :: Q Exp -> Q Exp
 syntax = fmap convertDo
 
