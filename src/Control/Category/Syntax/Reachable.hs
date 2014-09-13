6bbe10450b44732db249bd719345c4be5f97153c
Samuel Gélineau <gelisam+github@gmail.com>
2014-09-13 15:50:32 -0400

    extra case: reachable after weakening

--- src/Control/Category/Syntax/Reachable.hs
+++ src/Control/Category/Syntax/Reachable.hs
@@ -1,9 +1,30 @@
 module Control.Category.Syntax.Reachable where
 
 import Data.List
+import Language.Haskell.TH
 
 import Control.Category.Syntax.Types
 import Control.Category.Syntax.Vars
+
+
+sameSetAs :: Eq a => [a] -> [a] -> Bool
+sameSetAs xs ys = (intersect xs ys == xs)
+
+supersetOf :: Eq a => [a] -> [a] -> Bool
+supersetOf xs ys = all (`elem` xs) ys
+
+subsetOf :: Eq a => [a] -> [a] -> Bool
+subsetOf xs ys = supersetOf ys xs
+
+
+filterV :: (Name -> Bool) -> Vars -> Maybe Vars
+filterV p (Var x) | p x       = Just (Var x)
+                  | otherwise = Nothing
+filterV p (Pair x y) = case (filterV p x, filterV p y) of
+    (Just x', Just y') -> Just (Pair x' y')
+    (Just x', Nothing) -> Just x'
+    (Nothing, Just y') -> Just y'
+    (Nothing, Nothing) -> Nothing
 
 
 type ReachabilityTest = Vars -> Vars -> Bool
@@ -25,6 +46,13 @@ assocReachable :: ReachabilityTest
 assocReachable x y = (listVarNames x == listVarNames y)
 
 exchangeReachable :: ReachabilityTest
-exchangeReachable x y = sameSets (listVarNames x) (listVarNames y)
+exchangeReachable x y = listVarNames x `sameSetAs` listVarNames y
+
+weakenReachable :: ReachabilityTest -> ReachabilityTest
+weakenReachable reachable x y = xs `supersetOf` ys
+                             && case filterV (`elem` ys) x of
+                                  Just x' -> reachable x' y
+                                  Nothing -> False
   where
-    sameSets xs ys = (intersect xs ys == xs)
+    xs = listVarNames x
+    ys = listVarNames y
